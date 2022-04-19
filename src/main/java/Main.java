b7e315865b828005ae0dc267b6f0eb7fb6df7cce
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-19 13:55:46 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
+import java.awt.*;
+import java.sql.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int myAge=30;
-        int kavyaAge=myAge;
-        System.out.println(kavyaAge);
-    }
+        byte age=30;
+        long viewsCount = 1_123_456_789L;
+        float price = 30.11F;
+        char letter = 'A';
+        boolean isEligible=false;
+
+        byte x = 1;
+        byte y = x;
+        Point point1=new Point(x=1,y=1);
+      Point point2=point1;
+point1.x=2;
+        System.out.println(point2);
+       }
 }
