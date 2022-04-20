ab9b3aa2e49fc3e795e82becd743432e2ff4d9bb
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-20 15:09:48 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,22 @@
+import jdk.jshell.SourceCodeAnalysis;
+
 import java.awt.*;
 import java.sql.Date;
 import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-                     int role=1;
+String[] fruits ={"Aple", "mang","oran"};
+        for (String fruit : fruits)
+        System.out.println(fruit);
+        }
+    }
 
-                     switch (role) {
-                         case 1:
-                             System.out.println("you are admin");
 
-                         case 2:
-                             System.out.println("moderator");
-
-                         default:
-                             System.out.println("guest");
-                     }}}
 
 
 
