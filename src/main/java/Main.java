750054e34d7c8768b607fcb8cf4290800c6ec3bd
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-20 16:45:14 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-String[] fruits ={"Aple", "mang","oran"};
-        for (String fruit : fruits)
-        System.out.println(fruit);
+        final byte Percent = 100;
+        final byte Months_In_Year = 12;
+
+
+        int principal =0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Principal: ");
+            principal=scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
+
+        while (true) {
+            System.out.println("Annual interest rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest < 30) {
+                monthlyInterest = annualInterest / Percent / Months_In_Year;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        while (true) {
+            System.out.println("Period (Years):");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * Months_In_Year;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+           double mortgage=(principal*(monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments)-1));
+
+        String mortgageFormatted=NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: "+ mortgageFormatted);
+
         }
     }
 
