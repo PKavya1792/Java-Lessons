ff4d4c6a07a90e065c47f0f791abc7a4ec18add1
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-20 10:50:24 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,33 @@
 import java.awt.*;
 import java.sql.Date;
+import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+final byte Percent = 100;
+final byte Months_In_Year = 12;
 
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Name : ");
-        String name=scanner.nextLine();
-            System.out.println(" you are "+ name);
-                                              }
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Principal: ");
+int principal = scanner.nextInt();
+
+        System.out.println("Annual interest rate: ");
+float annualInterest= scanner.nextFloat();
+float monthlyInterest= annualInterest / Percent / Months_In_Year;
+
+        System.out.println("Period (Years):");
+        byte years=scanner.nextByte();
+        int numberOfPayments=years*Months_In_Year;
+
+        double mortgage=(principal*(monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments)-1));
+
+
+        String mortgageFormatted=NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: "+ mortgageFormatted);
+
+    }
 }
