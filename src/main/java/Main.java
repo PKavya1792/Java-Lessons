f1ac431f2f2c30adec51a1cc162b695c645ae5f0
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-20 11:12:20 -0400

    Intial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,27 +7,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-final byte Percent = 100;
-final byte Months_In_Year = 12;
+                    boolean hasGoodIncome=false;
+                    boolean hasCriminalRecord=false;
+                    boolean hasGoodCredit=true;
 
-Scanner scanner = new Scanner(System.in);
+                    boolean isEligible=(hasGoodIncome||hasGoodCredit) && !hasCriminalRecord;
 
-System.out.print("Principal: ");
-int principal = scanner.nextInt();
-
-        System.out.println("Annual interest rate: ");
-float annualInterest= scanner.nextFloat();
-float monthlyInterest= annualInterest / Percent / Months_In_Year;
-
-        System.out.println("Period (Years):");
-        byte years=scanner.nextByte();
-        int numberOfPayments=years*Months_In_Year;
-
-        double mortgage=(principal*(monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments)-1));
-
-
-        String mortgageFormatted=NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: "+ mortgageFormatted);
+                System.out.println(isEligible);
 
     }
 }
+
